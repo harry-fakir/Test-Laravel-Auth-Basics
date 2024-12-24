b69bd0b32abfce04af51583a98c29f085c75ee0c
Hernesha Fakir <harry@icehouseventures.co.nz>
2024-12-24 15:46:35 +1300

    laravel auth basics

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -17,8 +18,16 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            if ($request->filled('password')) {
+                $user->password = bcrypt($request->input('password'));
+            }
+            $user->save();
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
